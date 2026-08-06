ad1a11c45f8f51ad5cdbec45cb3d07683804ab75
Artem <artem-3112-artem@yandex.ru>
2026-08-06 22:32:27 +0300

    фикс гравит dto input

--- workspaces/server/src/auth/gravit/dto/inputs/gravit-authorize.input.ts
+++ workspaces/server/src/auth/gravit/dto/inputs/gravit-authorize.input.ts
@@ -1,27 +1,50 @@
-interface Password {
+import { IsBoolean, IsObject, IsOptional, IsString } from 'class-validator';
+import { Type } from 'class-transformer';
+import { ValidateNested } from 'class-validator';
+
+export class Password {
+  @IsString()
   password: string;
+  @IsString()
   type: string;
 }
 
-interface Password2FA {
-  firstPassword: {
-    password: string;
-    type: string;
-  };
-  secondPassword: {
-    totp: string;
-    type: string;
-  };
+export class FirstPasswordDto {
+  @IsString()
+  password: string;
+
+  @IsString()
+  type: string;
+}
+
+export class SecondPasswordDto {
+  @IsString()
+  totp: string;
+
+  @IsString()
+  type: string;
+}
+
+export class Password2FA {
+  @ValidateNested()
+  @Type(() => FirstPasswordDto)
+  firstPassword: FirstPasswordDto;
+
+  @ValidateNested()
+  @Type(() => SecondPasswordDto)
+  secondPassword: SecondPasswordDto;
 }
 
 export class GravitAuthorize {
+  @IsString()
   login: string;
-
+  @IsOptional()
+  @IsObject()
   context?: {
     ip: string;
   };
-
+  @IsObject()
   password: Password | Password2FA;
-
+  @IsBoolean()
   minecraftAccess: boolean;
 }
